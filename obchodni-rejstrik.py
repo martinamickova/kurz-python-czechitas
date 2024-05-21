89a6838a852ea06b27685e6fd76c1f59b4c51d1c
martinamickova <martina.mikulu@gmail.com>
2024-05-21 21:02:55 +0200

    obchodni rejstrik dodelana cast 2

--- obchodni-rejstrik.py
+++ obchodni-rejstrik.py
@@ -1,10 +1,24 @@
 import requests
 import json
 
-ico = input("Zadej IČO požadované firmy: ")
-path = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico
-response = requests.get(path)
-firma = response.json()
 
-print(firma['obchodniJmeno'])
-print(firma['sidlo']['textovaAdresa'])
+ico = input("Zadej IČO požadované firmy: ")
+path_ico = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico
+response_ico = requests.get(path_ico)
+firma_ico = response_ico.json()
+
+print(firma_ico['obchodniJmeno'])
+print(firma_ico['sidlo']['textovaAdresa'])
+
+
+name = input("Zadej název hledané firmy: ")
+path_name = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/vyhledat"
+headers = {"accept": "application/json",
+    "Content-Type": "application/json"}
+data = f'{{"obchodniJmeno": "{name}"}}'
+response_name = requests.post(path_name, headers=headers, data=data)
+firmy_name = response_name.json()
+
+print(f"Nalezeno subjektů: {firmy_name['pocetCelkem']}")
+for i in firmy_name['ekonomickeSubjekty']:
+    print(i['obchodniJmeno'], i['ico'])
