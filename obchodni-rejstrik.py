8e6a434001dc63e59d962dc8cc2e9dd194e98770
martinamickova <martina.mikulu@gmail.com>
2024-05-21 20:15:32 +0200

    domaci ukol obchodni rejstrik cast 1

---
+++ obchodni-rejstrik.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+ico = input("Zadej IČO požadované firmy: ")
+path = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico
+response = requests.get(path)
+firma: dict[str, Any] = response.json()
+
+print(firma['obchodniJmeno'])
+print(firma['sidlo']['textovaAdresa'])
